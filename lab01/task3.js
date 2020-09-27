48bd5127f4da90a89b7d48de256a82175651669a
theSawIsTheLaw <dimakrok@bk.ru>
2020-09-27 16:14:21 +0300

    FIN: lab01 is ready

--- lab01/task3.js
+++ lab01/task3.js
@@ -55,8 +55,8 @@ class DotsData
 
     getDistance(fDotName, sDotName)
     {
-        let fDot = this.dotsList.read(fDotName);
-        let sDot = this.dotsList.read(sDotName); 
+        let fDot = this.read(fDotName);
+        let sDot = this.read(sDotName); 
         if (fDot === null || sDot === null)
             return;
         
@@ -72,8 +72,8 @@ class DotsData
             return;
         let maxDistDots = [this.dotsList[0], this.dotsList[1]];
         let maxDist = this.getDistance(this.dotsList[0].name, this.dotsList[1].name);
-        for (fDot of this.dotsList)
-            for (sDot of this.dotsList)
+        for (let fDot of this.dotsList)
+            for (let sDot of this.dotsList)
             {
                 let dist = this.getDistance(fDot.name, sDot.name);
                 if (dist > maxDist)
@@ -88,7 +88,7 @@ class DotsData
 
     getDotsOnDistanceFromDot(mainDotName, distance)
     {
-        return this.dotsList.filter(dot => this.getDistance(mainDotName, dot) && this.getDistance(mainDotName, dot) <= distance)
+        return this.dotsList.filter(dot => this.getDistance(mainDotName, dot.name) && this.getDistance(mainDotName, dot.name) <= distance)
     }
 
     getDotsInCoordinateQuarter(axis, position)
@@ -103,10 +103,11 @@ class DotsData
         }
         else
         {
-            if (position.toLowerCase === "lower")
+            if (position.toLowerCase() === "lower")
                 filterfunc = dot => dot.yPos < 0;
-            else
+            else             
                 filterfunc = dot => dot.yPos > 0;
+                
         }
 
         return this.dotsList.filter(filterfunc);
@@ -121,6 +122,55 @@ class DotsData
 function main()
 {
     let dots = new DotsData();
+
+    dots.add("a", 10, 20);
+    dots.add("b", -10, 20);
+    dots.add("c", 10, -20);
+    dots.add("d", -10, -23);
+    dots.add("so far", 200, 300);
+    dots.add("uwu", 666, -888);
+
+    console.log("Current dots:");
+    dots.printOut();
+
+    // read test
+    console.log("Read dot d:", dots.read("d"));
+
+    // delete test
+    console.log("Delete dot uwu:");
+    console.log("Before:");
+    dots.printOut();
+    dots.del("uwu");
+    console.log("\nAfter:");
+    dots.printOut();
+
+    // upd tests
+    console.log("Upd of xPos of so far dot:");
+    dots.updX("so far", -11111111);
+    dots.printOut();
+    console.log("Upd of yPos of so far dot:");
+    dots.updY("so far", -2222);
+    dots.printOut();
+    console.log("Back total upd for so far dot:");
+    dots.upd("so far", 200, 300);
+    dots.printOut();
+
+    // Distant dots test
+    console.log("The most distant dots are:", dots.getMostDistantDots());
+
+    // Two points on neccessary constant
+    console.log("All dots distanted on 40 from d dot: ", dots.getDotsOnDistanceFromDot("d", 40));
+    console.log("All dots distanted on 100 from d dot: ", dots.getDotsOnDistanceFromDot("d", 100));
+
+    // Dot lower/heigher...
+    console.log("All dots lower x:", dots.getDotsInCoordinateQuarter("x", "lower"));
+    console.log("All dots heigher x:", dots.getDotsInCoordinateQuarter("x", "heigher"));
+    console.log("All dots lower y:", dots.getDotsInCoordinateQuarter("y", "lower"));
+    console.log("All dots heigher y:", dots.getDotsInCoordinateQuarter("y", "heigher"));
+    
+    // Dots in square
+    console.log("All dots in square 0, 0, 500, 400", dots.getDotsInSquare(500, 400, 0, 0));
+    console.log("All dots in square 100, 100, -100, -100", dots.getDotsInSquare(100, 100, -100, -100)); 
 }
 
 main(); 
