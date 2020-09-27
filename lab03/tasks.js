f64db7a901486d43bc7fc4f20565e0b67276733d
theSawIsTheLaw <dimakrok@bk.ru>
2020-09-27 22:16:41 +0300

    ADD: task2

--- lab03/tasks.js
+++ lab03/tasks.js
@@ -7,18 +7,52 @@ function task1()
     let validStrings = [];
     for (let i = 0; i < numOfStrings; i++)
     {
-        let str = readlineSync.question("String:");
+        let str = readlineSync.question("String: ");
         if (str.length % 2 === 0)
             validStrings.push(str);
     }
 
     let jsonValidStrings = JSON.stringify(validStrings);
-    fs.writeFileSync("task1out.txt", jsonValidStrings);
+    fs.writeFileSync("task1out.json", jsonValidStrings);
+}
+
+function task2()
+{
+    let fileName = readlineSync.question("Insert file name: ");
+
+    if (!fs.existsSync(fileName))
+    {
+        console.log("File doesn't exist. Going to next task...");
+        return;
+    }
+
+    let readContents = fs.readFileSync(fileName, "utf-8");
+    let strings = JSON.parse(readContents);
+
+    let consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j',
+                      'k', 'l', 'm', 'n', 'p', 'q', 'r',
+                      's', 't', 'v', 'w', 'x', 'y', 'z'];
+    let outString = [];
+    console.log("Strings made of vowels:");
+    for (let str of strings)
+    {
+        let stringValidityFlag = true;
+        for (let i = 0; i < consonants.length && stringValidityFlag; i++)
+            if (str.toLowerCase().includes(consonants[i]))
+                stringValidityFlag = false;
+        if (stringValidityFlag)
+            console.log(str);
+    }
+    console.log("...And that's all.");
 }
 
 function main()
 {
+    console.log("Task 1:");
     task1();
+
+    console.log("\n\nTask 2:");
+    task2();
 }
 
 const readlineSync = require('readline-sync');
